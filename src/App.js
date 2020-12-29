0dd067e7ed2d269ff354a1e26ade175d099cd7ef
jbp simandjuntak <jbpsimandjuntak@joannas-MacBook-Air.local>
2020-12-29 14:30:36 +0700

    added router to App.js, costumized city names in Header.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Home from "./containers/Home";
 function App() {
   return (
-    <div className="App bg-blue-200 h-screen flex items-center justify-center">
-      <header className="App-header">
-        <p className="text-2xl">
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-sm font-light"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is the weather-app starter code for Build a Website II
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+
+        <Route path="/my-name">
+          <h1>Hello</h1>
+          <p>You can call me Joanna</p>
+        </Route>
+
+        <Route path="/my-age">
+          <h1>18</h1>
+          <p>I was born on August 17th, 2002</p>
+        </Route>
+
+        <Route path="/where-i-live">
+          <h1>Jakarta</h1>
+          <p>Tomang</p>
+        </Route>
+
+      </Switch>
+    </Router>
   );
-}
+}      
 
 export default App;
