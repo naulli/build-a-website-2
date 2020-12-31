e264ef249940efdf6e41b31ec475560aa891f904
jbp simandjuntak <jbpsimandjuntak@joannas-MacBook-Air.local>
2020-12-31 14:31:40 +0700

    updated Details.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 import Home from "./containers/Home";
+import Details from "./containers/Details";
 
 function App() {
   return (
     <Router>
       <Switch>
+        {/* Details Route */}
+        <Route path="/city">
+          <Details />
+        </Route>
+
         <Route path="/">
           <Home />
         </Route>
       </Switch>
     </Router>
   );
-}      
+}
 
 export default App;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,24 +1,64 @@
 import React, { useState, useEffect, useMemo } from "react";
-// import axios from "axios";
-// import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
 import City from "../components/City";
 
-const weatherKey = `TODO`; // Your API Key here
-
 function Home() {
-  // TODO
+  const history = useHistory();
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Crema");
+
+  useEffect(() => {
+    // console.log(process.env.REACT_APP_WEATHER_KEY);
+    axios // axios is to get data from an API,you can also use axios to put/edit and delete data
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+      )
+      .then(function (response) {
+        // ‘promise’ (using .then and .catch)
+        // Succesful request
+        const weather = response.data;
+        setWeatherData(weather);
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.warn(error);
+      });
+  }, [city]); // the [city] is a dependency
+  // which u’re always listening to (aka always renewing and getting the last updated data)
+
+  useEffect(() => {
+    // useEffect is something that happens when u first open the page
+    const searchParams = history.location.search;
+    const urlParams = new URLSearchParams(searchParams);
+    const city = urlParams.get("city");
+    if (city) {
+      setCity(city);
+    }
+  }, [history]);
+
+  const { currentTemp } = useMemo(() => {
+    let currentTemp = "";
+    if (weatherData) {
+      currentTemp = `${Math.round(weatherData.main.temp)}°C`;
+    }
+    return {
+      currentTemp,
+    };
+  }, [weatherData]);
+
   return (
     // Container
     <div className="flex flex-col h-screen bg-green-200">
-      <City cityName={"Milan"} temp={"14°"} color={"bg-yellow-500"} />
+      <City cityName={city} temp={currentTemp} color={"bg-yellow-500"} />
 
-      <City cityName={"Paris"} temp={"10°"} color={"bg-pink-500"} />
+      <City cityName={city} temp={currentTemp} color={"bg-pink-500"} />
 
-      <City cityName={"Seoul"} temp={"7°"} color={"bg-purple-500"} />
+      <City cityName={city} temp={currentTemp} color={"bg-purple-500"} />
     </div>
   );
 }
